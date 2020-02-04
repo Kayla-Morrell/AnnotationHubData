39b579c297c9e053e2e4bd9d3c1f24934187532f
Kayla-Morrell <Kayla.Morrell@roswellpark.org>
2020-02-04 13:12:57 -0500

    Improvements made to `hub_create_package()`
    - Now there is a DESCRIPTION file template that contains the
    ideal information for a DESCRIPTION file
    - Allows user to define certain fields at the function call
    - Removal of some usethis functionality as it has wicked
    overwriting abilities

--- R/constructHubFunctions.R
+++ R/constructHubFunctions.R
@@ -6,37 +6,56 @@ use_scripts <- function(filename)
     edit_file(proj_path("inst/scripts", filename), open = FALSE)
 }
 
+create_description <- function(type, fields)
+{
+    fl <- system.file("rmarkdown", 
+        "templates",
+        "description", 
+        "skeleton",
+        "DESCRIPTION",
+        package = "AnnotationHubData")
+    tmpl <- readLines(fl)
+
+    if (type == "AnnotationHub") {
+        writeLines(whisker.render(tmpl, data = c(fields, 
+            list(biocViews = "AnnotationHub")
+        )), con = "DESCRIPTION")
+    }
+    else {
+        writeLines(whisker.render(tmpl, data = c(fields,
+            list(biocViews = "ExperimentHub")
+        )), con = "DESCRIPTION")
+    } 
+}
+
 hub_create_package <- function(package, 
     type = c("AnnotationHub", "ExperimentHub"),
-    fields = NULL, check_name = TRUE)
+    fields = list(packageName = path_file(package), 
+        version = "0.99.0", 
+        license = "Artistic-2.0"), 
+    check_name = TRUE)
 {
     stopifnot(
         length(package) == 1 && is.character(package),
         available_on_cran(package) == TRUE, 
-        available_on_bioc(package) == TRUE,
-        valid_package_name(package) == TRUE
+        available_on_bioc(package) == TRUE
     )
 
-    create_package(package, fields = fields, check_name = check_name)
+    create_package(package)
 
-    ## Customization of the DESCRIPTION file
-    use_description(fields = list(License = "Artistic-2.0",
-        LazyData = "false",
-        Version = "0.99.0"),
-        check_name = FALSE)
-    
+    ## Customization of the DESCRIPTION file (use whisker to render DESCRIPTION)
+    create_description(type, fields)
+
     use_package_doc()
     use_roxygen_md()
 
-    if (type == "AH") {
+    if (type == "AnnotationHub") {
         use_package("AnnotationHubData")
         use_package("AnnotationHub")
-        use_description(fields = list(biocViews =  "AnnotationHub"))
     }
     else {
         use_package("ExperimentHubData")
         use_package("ExperimentHub")
-        use_description(fields = list(biocViews = "ExperimentHub"))
     }
 
     ## Addition of README file
@@ -55,6 +74,16 @@ hub_create_package <- function(package,
     ## Addition of man/ directory
     use_directory("man")
 }
+    ## Addition of template csv file in inst/extdata
+#    file.create(paste0(getwd(),"/inst/extdata/metadata.csv"))
+#    df <- data.frame(matrix(ncol=17, nrow=0))
+#    x <- c("Title", "Description", "BiocVersion", "Genome", "SourceType", 
+#        "SourceUrl", "SourceVersion", "Species", "TaxonomyId", 
+#        "Coordinate_1_based", "DataProvider", "Maintainer", "RDataClass", 
+#        "DispatchClass", "Location_Prefix", "RDataPath", "Tags")
+#    colnames(df) <- x
+#    write.table(df, file = "/inst/extdata/metadata.csv", sep = ",",
+#        row.names = FALSE, col.names = TRUE)
 
 hub_create_resource <- function(package, title, description, biocversion, 
     genome, sourcetype, sourceurl, sourceversion, species, taxid, coordinate, 
